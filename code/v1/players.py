193f6c4b0117ef0faf0139b948a3954c600f0b45
Leia Tan <tan.woonsim@outlook.com>
2024-02-13 15:15:19 +0000

    select suit with min tiles as unwanted suit

--- code/v1/players.py
+++ code/v1/players.py
@@ -8,6 +8,14 @@ class Player(ABC):
     def __init__(self, possible_discards, displayed_tiles=TileList([])):
         self._displayed_tiles = displayed_tiles
         self._possible_discards = possible_discards
+        
+        tiles_by_suit = self.possible_discards.get_tiles_by_suit()
+        unwanted = list(tiles_by_suit.keys())[0]
+        for suit in tiles_by_suit.keys():
+            if tiles_by_suit[suit].size() < tiles_by_suit[unwanted].size():
+                unwanted = suit
+
+        self.unwanted_suit = unwanted
 
     @property
     def displayed_tiles(self):
@@ -35,10 +43,16 @@ class Player(ABC):
 
     def check_for_win(self, tile=None):
         all_tiles = self.all_tiles()
+        for t in all_tiles.tiles:
+            if t.suit_type == self.unwanted_suit:
+                return False
         return all_tiles.check_for_win(tile)
 
     def check_for_peng(self, tile):
-        return self._possible_discards.check_for_peng(tile)
+        if tile.suit_type != self.unwanted_suit:
+            return self._possible_discards.check_for_peng(tile)
+        else:
+            return False
 
     def choose_peng(self):
         return True
@@ -50,7 +64,13 @@ class Player(ABC):
         self._possible_discards.remove_tiles(l)
 
     def discard(self, game_state=None):
-        return self._possible_discards.remove_random_tile()
+        tiles_by_suit = self.possible_discards.get_tiles_by_suit()
+        if self.unwanted_suit not in tiles_by_suit.keys():
+            return self._possible_discards.remove_random_tile()
+        else:
+            tile = tiles_by_suit[self.unwanted_suit].remove_random_tile()
+            self._possible_discards.remove(tile)
+            return tile
 
     def get_hidden_tiles(self):
         return self.possible_discards
@@ -102,7 +122,7 @@ class SemiRandomAgent(Player):
         triples = [
             Tile(tile_str[:-1], tile_str[-1])
             for tile_str in counts.keys()
-            if counts[tile_str] >= 3
+            if counts[tile_str] >= 3 and tile_str[:-1] != self.unwanted_suit
         ]
         for triple in triples:
             l = TileList([triple for i in range(3)])
@@ -112,15 +132,16 @@ class SemiRandomAgent(Player):
     def lock_three_consecutive(self):
         self._possible_discards.sort()
         for tile in self.possible_discards.tiles:
-            second = Tile(tile.suit_type, str(int(tile.value) + 1))
-            third = Tile(tile.suit_type, str(int(tile.value) + 2))
-            if (
-                self.possible_discards.contains(tile)
-                and self.possible_discards.contains(second)
-                and self.possible_discards.contains(third)
-            ):
-                self._possible_discards.remove_tiles(TileList([tile, second, third]))
-                self._locked_tiles.add_tiles(TileList([tile, second, third]))
+            if tile.suit_type != self.unwanted_suit:
+                second = Tile(tile.suit_type, str(int(tile.value) + 1))
+                third = Tile(tile.suit_type, str(int(tile.value) + 2))
+                if (
+                    self.possible_discards.contains(tile)
+                    and self.possible_discards.contains(second)
+                    and self.possible_discards.contains(third)
+                ):
+                    self._possible_discards.remove_tiles(TileList([tile, second, third]))
+                    self._locked_tiles.add_tiles(TileList([tile, second, third]))
 
     def lock_triples(self):
         self.lock_three_of_a_kind()
@@ -132,7 +153,7 @@ class SemiRandomAgent(Player):
             pairs = [
                 Tile(tile_str[:-1], tile_str[-1])
                 for tile_str in counts.keys()
-                if counts[tile_str] >= 2
+                if counts[tile_str] >= 2 and tile_str[:-1] != self.unwanted_suit
             ]
             if len(pairs) > 0:
                 pair_index = random.randint(0, len(pairs) - 1)
@@ -148,22 +169,29 @@ class MCTSAgent(Player):
         self.simulations_to_run = simulations
 
     def discard(self, game_state):
-        # create state for mcts
-        # print("initialised with current player: ", game_state._current_player_number)
-        root = MonteCarloTreeSearchNode(game_state.initialise_mcts_state(), self.simulations_to_run)
-        selected_node = root.best_action()
 
-        if selected_node == -1:
-            # print("MCTS failed: discarding random tile")
-            return self._possible_discards.remove_random_tile()
+        tiles_by_suit = self.possible_discards.get_tiles_by_suit()
+        if self.unwanted_suit in tiles_by_suit.keys():
+            tile = tiles_by_suit[self.unwanted_suit].remove_random_tile()
+            self._possible_discards.remove(tile)
+            return tile
         else:
-            # print(
-            #     "action selected: discard", selected_node.parent_action[1].to_string()
-            # )
-            self._possible_discards.remove_tiles(
-                TileList([selected_node.parent_action[1]])
-            )
-            return selected_node.parent_action[1]
+            # create state for mcts
+            # print("initialised with current player: ", game_state._current_player_number)
+            root = MonteCarloTreeSearchNode(game_state.initialise_mcts_state(), self.simulations_to_run)
+            selected_node = root.best_action()
+
+            if selected_node == -1:
+                # print("MCTS failed: discarding random tile")
+                return self._possible_discards.remove_random_tile()
+            else:
+                # print(
+                #     "action selected: discard", selected_node.parent_action[1].to_string()
+                # )
+                self._possible_discards.remove_tiles(
+                    TileList([selected_node.parent_action[1]])
+                )
+                return selected_node.parent_action[1]
 
     def is_mcts(self):
         return True
